7f931ffc96dabac41aea4ca642dc630c6cc28cf7
Will S <fitnesswill13@gmail.com>
2022-10-22 21:26:17 -0700

    added prompts and got them to work

---
+++ index.js
@@ -0,0 +1,131 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
+
+
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'managerName',
+        message: "What is the team manager's name?",
+    },
+    {
+        type: 'input',
+        name: 'managerId',
+        message: "What is the manager's employee ID?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the manager's email address?"
+    },
+    {
+        type: 'input',
+        name: 'number',
+        message: "What is the manager's office phone number?",
+    },
+])
+.then((answers) => {
+    console.log(answers)
+    addOns()
+})
+
+const addOns = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'teamAdds',
+            message: "Who else is on this team?",
+            choices: ["I would like to add an Engineer", "I would like to add an intern", "I am done building my team"],
+        }
+    ])
+    .then((answer) => {
+        console.log(answer.teamAdds)
+    })
+}
+
+const teamMemberInfo = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: "What is the team members's name?",
+        },
+        {
+            type: 'input',
+            name: 'memberId',
+            message: "What is the team member's employee ID?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is this person's email address?"
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is this person's GitHub username?",
+        },
+    ])
+    .then((answers) => {
+        console.log(answers)
+    })
+    
+    }
+
+    const teamInternInfo = () => {
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'internName',
+                message: "What is the team intern's name?",
+            },
+            {
+                type: 'input',
+                name: 'internId',
+                message: "Please input this person's employee ID.",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is this person's email address?"
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "Where does this person go to school?",
+            },
+        ])
+        .then((answers) => {
+            console.log(answers)
+        })
+        
+        }
+
+
+
+
+
+
+/*GIVEN a command-line application that accepts user input
+WHEN I am prompted for my team members and their information
+THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+WHEN I click on an email address in the HTML
+THEN my default email program opens and populates the TO field of the email with the address
+WHEN I click on the GitHub username
+THEN that GitHub profile opens in a new tab
+WHEN I start the application
+THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+WHEN I enter the team manager’s name, employee ID, email address, and office number
+THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+WHEN I select the engineer option
+THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+WHEN I select the intern option
+THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+WHEN I decide to finish building my team
+THEN I exit the application, and the HTML is generated*/
